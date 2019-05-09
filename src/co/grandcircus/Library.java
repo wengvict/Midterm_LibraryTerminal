1bc86da44ca03dd22661b3511e3e89f430552c10
maryeddy <maryeddy0@gmail.com>
2019-05-09 13:52:25 -0400

    libraryclass&TestBookMain

---
+++ src/co/grandcircus/Library.java
@@ -0,0 +1,88 @@
+package co.grandcircus;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+
+	public static List<Book> readBookInv(String fileName) {
+
+		List<Book> bookList = new ArrayList<>();
+
+		Path filePath = Paths.get(fileName);
+		File f = filePath.toFile();
+		BufferedReader br = null;
+
+		try {
+			br = new BufferedReader(new FileReader(f));
+			String line = br.readLine();
+
+			while (line != null) {
+				String[] arr = line.split(",");
+				Book book = new Book(arr[0], arr[1], Boolean.parseBoolean(arr[2]), LocalDate.parse(arr[3]));
+				bookList.add(book);
+				line = br.readLine();
+			}
+
+			br.close();
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Book directory not found.");
+
+		} catch (IOException e) {
+			System.out.println("Something is wrong.");
+		}
+		return bookList;
+	}
+
+	public static void addBook(List<Book> list) {
+		String fileName = "books.txt";
+		Path path = Paths.get(fileName);
+
+		// Path to.txt file
+		File file = path.toFile();
+		// initializing printwriter
+		PrintWriter output = null;
+
+		try {
+			output = new PrintWriter(new FileOutputStream(file, true));
+
+			// takes list looks for book objects to put into
+			for (Book b : list) {
+				output.println(b);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Cant add to list");
+		} finally {
+			// insurance - not more alterations to .txt
+			output.close();
+		}
+	}
+
+	public static void findByName(List<Book> list, String userSearchAuthor ) {
+
+		for (int i = 0; i < list.size(); i++) {
+			String s = list.get(i).getAuthor();
+		
+			if(s.contains(userSearchAuthor)) {
+			System.out.println(list.get(i));
+			}else {
+				
+			}
+			
+		}
+		
+		
+	}
+
+}
---
+++ src/co/grandcircus/Test_BookMain.java
@@ -0,0 +1,15 @@
+package co.grandcircus;
+
+public class Test_BookMain {
+
+	public static void main(String[] args) {
+		
+	
+		
+	Library.readBookInv("books.txt");		
+		
+	//Library.findByName(Library.readBookInv("books.txt")	, "Dickens");
+		
+	}
+
+}
